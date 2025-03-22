83ab581ca16730ad88709dc7dc42dbf0ccb24083
Nikolai Zhulikov <nzhulikov@gmail.com>
2025-03-22 22:43:15 +0100

    refactored and added nl language

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import json
 import tempfile
 import logging
 from hashlib import sha256
-from telebot.types import ReplyParameters
+from telebot.types import ReplyParameters, Message, MenuButtonCommands, BotCommand, BotCommandScopeChat
 
 from anki.collection import Collection, AddNoteRequest
 
@@ -21,6 +21,13 @@ DEFAULT_ANKI = False
 logger.info("Initializing Telegram bot and OpenAI client...")
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 client = openai.OpenAI(api_key=OPENAI_API_KEY)
+available_languages = {
+    "gr": "греческий",
+    "sb": "сербский",
+    "en": "английский",
+    "nl": "нидерландский",
+}
+
 
 def translate_text(text, settings):
     """Uses ChatGPT to translate and structure text into standard Greek while keeping original phrases."""
@@ -72,49 +79,8 @@ def save_settings(chat_dir, settings, **kwargs):
     with open(f"{chat_dir}/settings.json", "w") as f:
         json.dump(settings, f)
 
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    try:
-        logger.info(f"Received message from chat {message.chat.id}: {message.text[:50]}...")
-        _handle_message(message)
-    except Exception as e:
-        logger.exception("Error processing message")
-        raise e
 
-def _handle_message(message):
-    # create directory for this chat if not exists
-    chat_dir = str(message.chat.id)
-    
-    if message.text == "/start":
-        logger.debug("Processing /start command")
-        bot.send_message(message.chat.id, "Привет! Отправь мне текст, и я переведу его, а затем создам для тебя колоду для Anki.\n"
-                                         "Команда /help покажет доступные команды.")
-        
-        if os.path.exists(chat_dir):
-            for file in os.listdir(chat_dir):
-                os.remove(f"{chat_dir}/{file}")
-        else:
-            os.makedirs(chat_dir)
-        return
-    
-    if message.text == "/help":
-        bot.send_message(message.chat.id, "Отправь мне текст, и я переведу его, а затем создам для тебя колоду для Anki.\n"
-                                         "Используй короткий текст, ограничение 1000 символов.\n"
-                                         "Доступные команды:\n"
-                                         "/start - начать работу с ботом/сбросить настройки\n"
-                                         "/lang - текущий язык перевода\n"
-                                         "/gr - изменить язык на греческий\n"
-                                         "/sb - изменить язык на сербский\n"
-                                         "/en - изменить язык на английский\n"
-                                         "/anki - изменить режим на подготовку колоды для Anki\n"
-                                         "/chat - изменить режим на отправку переводов в чат\n"
-                                         "/help - показать это сообщение\n"
-                                         )
-        return
-
-    if not os.path.exists(chat_dir):
-        bot.send_message(message.chat.id, "Сначала отправь /start")
-        return
+def get_settings(chat_dir):
     settings = {"language": DEFAULT_LANGUAGE, "anki": DEFAULT_ANKI}
     if not os.path.exists(f"{chat_dir}/settings.json"):
         with open(f"{chat_dir}/settings.json", "w") as f:
@@ -122,66 +88,167 @@ def _handle_message(message):
     else:
         with open(f"{chat_dir}/settings.json", "r") as f:
             settings = {**settings, **json.load(f)}
+    return settings
 
-    if message.text == "/lang":
-        bot.send_message(message.chat.id, f"Я перевожу текст на {settings['language']} язык.")
-        return
-    if message.text == "/gr":
-        save_settings(chat_dir, settings, language="греческий")
-        bot.send_message(message.chat.id, "Теперь я перевожу текст на греческий язык.")
-        return
-    if message.text == "/sb":
-        save_settings(chat_dir, settings, language="сербский")
-        bot.send_message(message.chat.id, "Теперь я перевожу текст на сербский язык.")
-        return
-    if message.text == "/en":
-        save_settings(chat_dir, settings, language="английский")
-        bot.send_message(message.chat.id, "Теперь я перевожу текст на английский язык.")
-        return
-    if message.text == "/anki":
-        save_settings(chat_dir, settings, anki=True)
-        bot.send_message(message.chat.id, "Теперь я буду готовить колоду для Anki.")
-        return
-    if message.text == "/chat":
-        save_settings(chat_dir, settings, anki=False)
-        bot.send_message(message.chat.id, "Теперь я буду скидывать переводы в чат.")
-        return
+
+def init_commands(chat_id):
+    bot.set_chat_menu_button(
+        chat_id=chat_id,
+        menu_button=MenuButtonCommands()
+    )
+    bot.set_my_commands(
+        commands=[
+            BotCommand(command="start", description="Начать работу с ботом"),
+            BotCommand(command="settings", description="Настройки бота"),
+            BotCommand(command="help", description="Показать доступные команды"),
+        ],
+        scope=BotCommandScopeChat(chat_id=chat_id)
+    )
+
+def init_language_commands(chat_id):
+    commands = [BotCommand(command=command, description=f"Изменить язык на {language}") for command,language in available_languages.items()]
+    commands.append(BotCommand(command="back", description="Вернуться к выбору настроек"))
+    bot.set_my_commands(
+        commands=commands,
+        scope=BotCommandScopeChat(chat_id=chat_id)
+    )
+
+
+def init_settings_commands(chat_id):
+    bot.set_my_commands(
+        commands=[
+            BotCommand(command="anki", description="Изменить режим на подготовку колоды для Anki"),
+            BotCommand(command="chat", description="Изменить режим на отправку переводов в чат"),
+            BotCommand(command="lang", description="Изменить текущий язык перевода"),
+            BotCommand(command="back", description="Вернуться к главному меню"),
+        ],
+        scope=BotCommandScopeChat(chat_id=chat_id)
+    )
+
+
+@bot.message_handler(commands=["back"])
+def handle_back_command(message: Message):
+    current_commands = bot.get_my_commands()
+    if current_commands[0].command in available_languages.keys():
+        init_language_commands(message.chat.id)
+    else:
+        init_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["start"])
+def handle_start(message: Message):
+    chat_dir = str(message.chat.id)
+    if os.path.exists(chat_dir):
+        for file in os.listdir(chat_dir):
+            os.remove(f"{chat_dir}/{file}")
+    else:
+        os.makedirs(chat_dir)
+    init_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["settings"])
+def handle_settings(message: Message):
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
+    sentences = []
+    if settings["anki"]:
+        sentences.append("Я буду готовить колоду для Anki.")
+    else:
+        sentences.append("Я буду отправлять переводы в чат.")
+    sentences.append(f"Я перевожу текст на {settings['language']} язык.")
+    bot.send_message(message.chat.id, '\n'.join(sentences))
+    init_settings_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["lang"])
+def handle_lang(message: Message):
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
+    bot.send_message(message.chat.id, f"Я перевожу текст на {settings['language']} язык.")
+    init_language_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["gr", "sb", "en", "nl"])
+def handle_set_language(message: Message):
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
+    language = available_languages[message.text[1:]]
+    save_settings(chat_dir, settings, language=language)
+    bot.send_message(message.chat.id, f"Теперь я перевожу текст на {language} язык.")
+    init_settings_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["anki"])
+def handle_anki(message: Message):
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
+    save_settings(chat_dir, settings, anki=True)
+    bot.send_message(message.chat.id, "Теперь я буду готовить колоду для Anki.")
+    init_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["chat"])
+def handle_chat(message: Message):
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
+    save_settings(chat_dir, settings, anki=False)
+    bot.send_message(message.chat.id, "Теперь я буду отправлять переводы в чат.")
+    init_commands(message.chat.id)
+
+
+@bot.message_handler(commands=["help"])
+def handle_help(message: Message):
+    sentences = [
+        "Отправь мне текст, и я переведу его и озвучу. Ограничение 1000 символов.",
+        "Доступные команды:",
+        "/start - начать работу с ботом/сбросить настройки",
+        "/settings - настройки бота",
+        "/anki - изменить режим на подготовку колоды для Anki",
+        "/chat - изменить режим на отправку переводов в чат",
+        "/lang - текущий язык перевода",
+        *[f"/{command} - изменить язык на {language}" for command, language in available_languages.items()],
+        "/help - показать это сообщение"
+    ]
+    bot.send_message(message.chat.id, '\n'.join(sentences))
+    init_commands(message.chat.id)
+
+
+@bot.message_handler()
+def handle_message(message: Message):
+    init_commands(message.chat.id)
+    chat_dir = str(message.chat.id)
+    settings = get_settings(chat_dir)
     if len(message.text) > 1000:
         bot.send_message(message.chat.id, "Текст слишком длинный, попробуй меньше 1000 символов.")
         return
 
-    status_message = bot.send_message(message.chat.id, "Перевожу текст...")
     with tempfile.TemporaryDirectory() as tmpdir:
-        bot.send_chat_action(message.chat.id, "typing")
+        bot.send_chat_action(message.chat.id, "upload_document" if settings["anki"] else "typing", timeout=60)
         translated_text = translate_text(message.text, settings)
-        if len(translated_text) > 1000:
-            bot.edit_message_text("Получился слишком длинный текст, попробуйте снова.", message.chat.id, status_message.id)
+        if len(translated_text) > 1500:
+            bot.send_message("Получился слишком длинный текст, попробуйте снова.", message.chat.id)
             return
 
         if not settings["anki"]:
-            translation_message_id = status_message.id
-            bot.edit_message_text(translated_text, message.chat.id, translation_message_id)
-            status_message = bot.send_message(message.chat.id, "Озвучиваю текст...")
-            bot.send_chat_action(message.chat.id, "record_voice")
+            translation_message = bot.send_message(message.chat.id, translated_text,
+                reply_parameters=ReplyParameters(message.id, allow_sending_without_reply=True))
+            bot.send_chat_action(message.chat.id, "record_voice", timeout=60)
             audio_filename = f"{tmpdir}/audio_{sha256(translated_text.encode()).hexdigest()}.mp3"
             synthesize_speech(translated_text, audio_filename)
             with open(audio_filename, "rb") as audio:
-                bot.send_voice(message.chat.id, audio, reply_parameters=
-                            ReplyParameters(translation_message_id, allow_sending_without_reply=True))
-                bot.delete_message(message.chat.id, status_message.id)
+                bot.send_voice(message.chat.id, audio,
+                    reply_parameters=ReplyParameters(translation_message.id, allow_sending_without_reply=True))
             return
         
         lines = translated_text.strip().split("\n")
         lines = [line for line in lines if ';' in line]
 
         if not lines:
-            bot.edit_message_text("Не получилось перевести текст, попробуйте снова.", message.chat.id, status_message.id)
+            bot.send_message("Не получилось перевести текст, попробуйте снова.", message.chat.id)
             return
 
         deck_name = lines[0].split(";")[0].strip()
         anki_package_filename = f"{tmpdir}/{deck_name}.apkg"
-        bot.edit_message_text("Озвучиваю текст...", message.chat.id, status_message.id)
-        bot.send_chat_action(message.chat.id, "record_voice")
         collection = Collection(f"{tmpdir}/collection.anki2")
         try:
             deck_id = collection.decks.add_normal_deck_with_name(deck_name).id
@@ -199,9 +266,6 @@ def _handle_message(message):
                 note.fields = [original, f"{translated}[sound:{mp3_filename}]"]
                 note.tags = ["эссе"]
                 add_note_requests.append(AddNoteRequest(note=note, deck_id=deck_id))
-
-            bot.edit_message_text("Собираю колоду...", message.chat.id, status_message.id)
-            bot.send_chat_action(message.chat.id, "typing")
             
             collection.add_notes(add_note_requests)
             collection.export_anki_package(
@@ -214,8 +278,5 @@ def _handle_message(message):
         finally:
             collection.close()
 
-        bot.edit_message_text("Отправляю колоду...", message.chat.id, status_message.id)
-        bot.send_chat_action(message.chat.id, "upload_document")
         with open(anki_package_filename, "rb") as zipf:
-            bot.send_document(message.chat.id, zipf)
-        bot.delete_message(message.chat.id, status_message.id)
+            bot.send_document(message.chat.id, zipf, reply_parameters=ReplyParameters(message.id, allow_sending_without_reply=True))
